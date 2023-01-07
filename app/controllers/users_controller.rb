2f914e6d696253f82056ae5e6f3b57aee9429244
David Verghese <dj.verghese@gmail.com>
2023-01-07 13:38:49 -0500

    bring back auth for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user, only: [:create, :index]
+  skip_before_action :authenticate_user, only: [:create]
 
   def index
     @users = User.all
