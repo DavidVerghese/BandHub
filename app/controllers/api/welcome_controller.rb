3ecaa14386e71010c0e15b07a4ce0b3146c03020
David Verghese <dj.verghese@gmail.com>
2023-01-20 17:50:05 -0500

    Testing to see if changes show up on deployed site

--- app/controllers/api/welcome_controller.rb
+++ app/controllers/api/welcome_controller.rb
@@ -1,6 +1,6 @@
 class Api::WelcomeController < ApplicationController
   skip_before_action :authenticate_user
   def index 
-    render json: {message: "Welcome to my site!"}
+    render json: {message: "Welcome to my site!!"}
   end
 end
