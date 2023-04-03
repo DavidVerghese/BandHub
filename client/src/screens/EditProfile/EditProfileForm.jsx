0e99211be6617b072a0e39e8fc0e773048124186
David Verghese <dj.verghese@gmail.com>
2023-04-03 10:37:47 -0400

    fix issue with edit form not working on deployed site

--- client/src/screens/EditProfile/EditProfileForm.jsx
+++ client/src/screens/EditProfile/EditProfileForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import './EditProfileForm.css';
@@ -12,10 +12,26 @@ function EditProfileForm({ user, setUser }) {
   const [name, setName] = useState(user.name);
   const [email, setEmail] = useState(user.email_address);
   const [pictureUrl, setPictureUrl] = useState(user.picture_url);
-  const [genre, setGenre] = useState(user.genre.name);
-  const [instrument, setInstrument] = useState(user.instrument.name);
-  const [location, setLocation] = useState(user.location.name);
-  const [lookingFor, setLookingFor] = useState(user.looking_for.name);
+  
+  // const [genre, setGenre] = useState(user.genre.name);
+  // const [instrument, setInstrument] = useState(user.instrument.name);
+  // const [location, setLocation] = useState(user.location.name);
+  // const [lookingFor, setLookingFor] = useState(user.looking_for.name);
+
+    const [genre, setGenre] = useState([]);
+  const [instrument, setInstrument] = useState([]);
+  const [location, setLocation] = useState([]);
+  const [lookingFor, setLookingFor] = useState([]);
+  
+  useEffect(() => {
+    const { genre, instrument, location, looking_for } = user;
+
+    setGenre(genre.name);
+    setInstrument(instrument.name);
+    setLocation(location.name);
+    setLookingFor(looking_for.name);
+
+  }, [user]);
 
   console.log(user);
   function handleSubmit(event) {
@@ -73,41 +89,42 @@ function EditProfileForm({ user, setUser }) {
         <Form.Control type="text" value={pictureUrl} onChange={(event) => setPictureUrl(event.target.value)} />
       </Form.Group>
 
-      <Form.Group controlId="genre">
+        {genre &&  <Form.Group controlId="genre">
       <Form.Label>Genre:</Form.Label>
         <Form.Select value={genre} aria-label="Default select example" onChange={(event) => setGenre(event.target.value)}>
       {genres.map((genre) => (
         <option key={genre.name} value={genre.name}>{genre.name}</option>
       ))}
         </Form.Select>
-      </Form.Group>
+      </Form.Group>}
+     
 
-      <Form.Group controlId="instrument">
-        <Form.Label>Instrument:</Form.Label>
-        <Form.Select value={instrument} aria-label="Default select example"  onChange={(event) => setInstrument(event.target.value)}>
-        {instruments.map((instrument) => (
-        <option key={instrument.name} value={instrument.name}>{instrument.name}</option>
-      ))}
-    </Form.Select>
-      </Form.Group>
+        {instrument && <Form.Group controlId="instrument">
+          <Form.Label>Instrument:</Form.Label>
+          <Form.Select value={instrument} aria-label="Default select example" onChange={(event) => setInstrument(event.target.value)}>
+            {instruments.map((instrument) => (
+              <option key={instrument.name} value={instrument.name}>{instrument.name}</option>
+            ))}
+          </Form.Select>
+        </Form.Group>}
 
-      <Form.Group controlId="location">
-        <Form.Label>Location:</Form.Label>
-        <Form.Select value={location} onChange={(event) => setLocation(event.target.value)}>
-          {locations.map((location) => (
-          <option key={location.name}>{location.name}</option>
-      ))}
-    </Form.Select>
-      </Form.Group>
+        {location && <Form.Group controlId="location">
+          <Form.Label>Location:</Form.Label>
+          <Form.Select value={location} onChange={(event) => setLocation(event.target.value)}>
+            {locations.map((location) => (
+              <option key={location.name}>{location.name}</option>
+            ))}
+          </Form.Select>
+        </Form.Group>}
 
-      <Form.Group controlId="lookingFor">
-        <Form.Label>Looking For:</Form.Label>
-        <Form.Select value={lookingFor} aria-label="Default select example"  onChange={(event) => setLookingFor(event.target.value)}>
-        {instruments.map((instrument) => (
-        <option key={instrument.name} value={instrument.name}>{instrument.name}</option>
-      ))}
-    </Form.Select>
-      </Form.Group>
+        {lookingFor && <Form.Group controlId="lookingFor">
+          <Form.Label>Looking For:</Form.Label>
+          <Form.Select value={lookingFor} aria-label="Default select example" onChange={(event) => setLookingFor(event.target.value)}>
+            {instruments.map((instrument) => (
+              <option key={instrument.name} value={instrument.name}>{instrument.name}</option>
+            ))}
+          </Form.Select>
+        </Form.Group>}
 
       <Button variant="primary" type="submit">Save</Button>
       </Form>
